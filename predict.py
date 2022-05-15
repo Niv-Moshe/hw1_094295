e743883fb75f0adf2ca70c3f9c7a91e0e65715d2
NivMoshe2000 <niv.moshe@campus.technion.ac.il>
2022-05-15 17:23:51 +0300

    final mlp

---
+++ predict.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+from tensorflow import keras
+
+
+
+def predict(test_directory_path, features_to_use=used_features):
+    model = keras.models.load_model('lstm_model.h5', custom_objects={"f1_score_mine": f1_score_mine})
+    ########### Test: #############
+    print()
+    print("Testing...")
+    _, _, test_df_list = read_data(get_filenames(test_directory_path))
+    print("Pre-processing")
+    x_test, y_test, test_pids = dfs_to_matrix(test_df_list, features_to_use)
+    print(f"Test shapes: {x_test.shape=}, {y_test.shape=}")
+    del test_df_list
+    print("Finished filling missing values in test")
+    y_one_hot = []  # one hot vector indicates on label
+    for y in y_test:
+        if y == 0:
+            y_one_hot.append([1, 0])
+        else:
+            y_one_hot.append([0, 1])
+    y_one_hot_test = np.array(y_one_hot)
+    y_pred = model.predict(x_test)
+    y_preds = [np.argmax(ys) for ys in y_pred]
+    y_act = [np.argmax(ys) for ys in y_one_hot_test]
+
+    pred_df = pd.DataFrame(data={'Id': test_pids, 'SepsisLabel': y_preds})
+    pred_df.sort_values(by='Id', ascending=True, inplace=True)
+    pred_df.to_csv('prediction.csv', index=False)
+
+
+    pass
